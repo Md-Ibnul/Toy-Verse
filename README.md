42085e8ef7d343c2d9e9cb33165ca968e5adac60
Md-Ibnul <ibnulhasan456@gmail.com>
2023-05-21 18:10:59 +0600

    Extra section fixed

---
+++ README.md
@@ -0,0 +1,53 @@
+# Ranna. Repository
+
+ Best Toy Store website where you can find more unique superhero toys. This is very amazing website.
+
+## Features of this site
+1. Awesome User Interface
+2. Mobile Responsive
+3. More Details
+4. All Excellent Toys
+5. All Special Collects
+6. Login and Sign Up 
+7. Add New Toys
+8. See Add Toys
+9. Ready to use.
+
+## Live Link
+Hosted in Netlify -> [ToyVerse](https://toys-verse.web.app/)
+
+
+## Complete Project
+Github Repo -> [ToyVerse-complete](https://github.com/programming-hero-web-course-4/b7a11-toy-marketplace-client-side-Md-Ibnul)
+
+
+## React Router Dom v6.4 
+Documentation Link -> [Link](https://reactrouter.com/en/main/start/overview)
+
+## Firebase
+Documentation Link -> [Link](https://firebase.google.com/)
+
+## MongoDB
+Documentation Link -> [Link](https://www.mongodb.com/)
+
+## Tailwind CSS
+Documentation Link -> [Link](https://tailwindcss.com/docs/installation)
+
+## DaisyUI
+Documentation Link -> [Link](https://daisyui.com/)
+
+## AOS
+Documentation Link -> [Link](https://michalsnik.github.io/aos/)
+
+## Resources & Credits
+
+Banner Image -> [Google](https://www.google.com/)
+
+Icon -> [React Icon](https://react-icons.github.io/react-icons/)
+
+Tab -> [React Tab](https://www.npmjs.com/package/react-tabs)
+
+
+Image -> [Freepik](https://www.freepik.com/)
+
+
--- src/Pages/Home/Components/Other.jsx
+++ src/Pages/Home/Components/Other.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
-import other from '../../../assets/Other.jpg'
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import { useEffect } from "react";
+import original from '../../../assets/original.png'
+import offer from '../../../assets/offer.jpg'
+import delivery from '../../../assets/delivery.png'
+import customer from '../../../assets/customer support.jpg'
 
 const Other = () => {
+    useEffect(() => {
+        AOS.init({duration: 2500});
+      },[]);
     return (
-        <div className='my-container my-16'>
-            <img src={other} className='w-fit' alt="Image" />
+        <div className='my-container my-16 md:my-24'>
+            <div className='md:flex'>
+                <div className='w-fit mx-auto md:w-1/4 mb-10 md:mb-0' data-aos="fade-right">
+                    <img className='w-60 md:mt-10' src={original} alt="" />
+                </div>
+                <div className='w-fit mx-auto md:w-1/4 mb-10 md:mb-0' data-aos="fade-down-right">
+                    <img className='w-64 md:mt-14' src={offer} alt="" />
+                </div>
+                <div className='w-fit mx-auto md:w-1/4 mb-20 md:mb-0' data-aos="fade-down-left">
+                    <img className='w-64 md:mt-16' src={delivery} alt="" />
+                </div>
+                <div className='w-fit mx-auto md:w-1/4' data-aos="fade-left">
+                    <img className='w-64' src={customer} alt="" />
+                </div>
+            </div>
         </div>
     );
 };
